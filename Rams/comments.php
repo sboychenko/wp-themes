3da2b33f9b583d0198f13e120bb1b0955a0f23a9
sboichenko <sboichenko@at-consulting.ru>
2016-09-28 16:14:40 +0300

    fork v1.15 https://wordpress.org/themes/rams/

---
+++ Rams/comments.php
@@ -0,0 +1,121 @@
+<?php if ( post_password_required() ) return; ?>
+
+<?php if ( have_comments() ) : ?>
+
+	<a name="comments"></a>
+	
+	<div class="comments-title-container">
+		
+		<h2 class="comments-title fleft">
+		
+			<?php echo count($wp_query->comments_by_type[comment]) . ' ';
+			echo _n( 'Comment' , 'Comments' , count($wp_query->comments_by_type[comment]), 'rams' ); ?>
+			
+		</h2>
+		
+		<h4 class="comments-subtitle fright"><a href="#respond"><?php _e('Add yours','rams'); ?> &rarr;</a></h4>
+		
+		<div class="clear"></div>
+	
+	</div> <!-- /comments-title-container -->
+
+	<div class="comments">
+
+		<ol class="commentlist">
+		    <?php wp_list_comments( array( 'type' => 'comment', 'callback' => 'rams_comment' ) ); ?>
+		</ol>
+		
+		<?php if (!empty($comments_by_type['pings'])) : ?>
+		
+			<div class="pingbacks">
+			
+				<div class="pingbacks-inner">
+			
+					<h3 class="pingbacks-title">
+					
+						<?php echo count($wp_query->comments_by_type[pings]) . ' ';
+						echo _n( 'Pingback', 'Pingbacks', count($wp_query->comments_by_type[pings]), 'rams' ); ?>
+					
+					</h3>
+				
+					<ol class="pingbacklist">
+					    <?php wp_list_comments( array( 'type' => 'pings', 'callback' => 'rams_comment' ) ); ?>
+					</ol>
+					
+				</div>
+				
+			</div>
+		
+		<?php endif; ?>
+				
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
+			
+			<div class="comments-nav" role="navigation">
+			
+				<div class="fleft">
+									
+					<?php previous_comments_link( '&laquo; ' . __( 'Older Comments', 'rams' ) ); ?>
+				
+				</div>
+				
+				<div class="fright">
+				
+					<?php next_comments_link( __( 'Newer Comments', 'rams' ) . ' &raquo;' ); ?>
+				
+				</div>
+				
+				<div class="clear"></div>
+				
+			</div> <!-- /comment-nav-below -->
+			
+		<?php endif; ?>
+		
+	</div> <!-- /comments -->
+	
+<?php endif; ?>
+
+<?php if ( ! comments_open() && ! is_page() ) : ?>
+
+	<p class="no-comments"><?php _e( 'Comments are closed.', 'rams' ); ?></p>
+	
+<?php endif; ?>
+
+<?php $comments_args = array(
+
+	'comment_notes_before' => 
+		'',
+		
+	'comment_notes_after' =>
+		'',
+
+	'comment_field' => 
+		'<p class="comment-form-comment">
+			<label for="comment">' . __('Comment','rams') . '</label>
+			<textarea id="comment" name="comment" cols="45" rows="6" required></textarea>
+		</p>',
+	
+	'fields' => apply_filters( 'comment_form_default_fields', array(
+	
+		'author' =>
+			'<p class="comment-form-author">
+				<label for="author">' . __('Name','rams') . ( $req ? '<span class="required">*</span>' : '' ) . '</label> 
+				<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30" />
+			</p>',
+		
+		'email' =>
+			'<p class="comment-form-email">
+				<label for="email">' . __('Email','rams') . ( $req ? '<span class="required">*</span>' : '' ) . '</label> 
+				<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30" />
+			</p>',
+		
+		'url' =>
+			'<p class="comment-form-url">
+				<label for="url">' . __('Website','rams') . '</label>
+				<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" />
+			</p>')
+	),
+);
+
+comment_form($comments_args);
+
+?>
---
+++ Rams/readme.txt
@@ -0,0 +1,54 @@
+# Copyright
+--------------
+
+Rams WordPress Theme, Copyright 2015 Anders Norén
+Rams is distributed under the terms of the GNU GPL v2
+
+
+
+# Install Steps
+--------------
+
+1. Upload the theme.
+2. Activate the theme.
+
+
+
+# Use the gallery post format
+--------------
+
+1. Go to Admin > Posts > Add New.
+2. Select the "Gallery" post format in the Post Attributes box.
+3. Click "Add Media" and upload the images you wish to display in the gallery.
+4. Close the Media window and publish/update the post.
+5. The images you uploaded should now be displayed in the post gallery.
+
+
+
+# Use the the quote post format
+--------------
+
+1. Create a new post.
+2. Select "Quote" in the Format window to the right.
+3. In the post content, enter the quote content within a blockquote element, and the quote attribution within a cite element.
+4. Directly after the two elements, add the <!--more--> tag followed by the rest of the content. Example:
+
+<blockquote>[quote content]
+<cite>[quote attribution]</cite>
+</blockquote>
+<!--more-->
+The rest of the content...
+
+5. Publish.
+6. The blockquote will now be presented as a single link element on the archive pages.
+
+
+
+# Licenses
+--------------
+
+Montserrat font license : SIL Open Font License, 1.1 http://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL
+
+Crimson Text font license : SIL Open Font License, 1.1 http://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL
+
+FlexSlider jQuery slider license : GNU GPL v2.0 http://www.gnu.org/licenses/gpl-2.0.html
